e54d42c213e497d36269e1d0e5ec607570d1a07b
jasminesloan <jasminesloan@fullsail.edu>
2012-11-28 22:31:35 -0500

    started code all over, can't figure out the operators, there not working.

---
+++ Week 2/Calculator/Calculator/infoViewController.m
@@ -0,0 +1,38 @@
+//
+//  infoViewController.m
+//  Calculator
+//
+//  Created by Jasmine Jamieson on 11/28/12.
+//  Copyright (c) 2012 com.fullsail. All rights reserved.
+//
+
+#import "infoViewController.h"
+
+@interface infoViewController ()
+
+@end
+
+@implementation infoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
